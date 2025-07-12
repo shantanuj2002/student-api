f2e9d88c70a51734b9b2042528f7e88deac6d51a
Shantanu Joshi <joshishantanu21@gmail.com>
2025-07-12 11:06:29 +0530

    server shutdown handel

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,36 +1,57 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/shantanuj2002/students-api/internal/config"
 )
 
 func main() {
-	// load config
+	// Load config
 	cfg := config.MustLoad()
-	//database setup
 
-	//setup router
-
+	// Set up router
 	router := http.NewServeMux()
-
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to stident api"))
+		w.Write([]byte("Welcome to student api"))
 	})
-	//setup server
 
-	server := http.Server{
+	// Create server
+	server := &http.Server{
 		Addr:    cfg.Addr,
 		Handler: router,
 	}
 
-	fmt.Printf("server start %s", cfg.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("fail to start server")
+	// Channel to listen for interrupt/terminate signals
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	// Run server in a goroutine
+	go func() {
+		fmt.Printf("Server started at %s\n", cfg.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+
+	// Block until signal is received
+	<-stop
+	slog.Info("\nShutting down server...")
+
+	// Graceful shutdown with timeout context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Server Shutdown Failed: %v", err)
 	}
 
 }
